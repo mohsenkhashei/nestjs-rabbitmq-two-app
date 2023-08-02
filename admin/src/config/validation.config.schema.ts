72cbfcea8670f4a16b7b4aed9b4c5da95a8a3acc
MohsenKhashei <m.khasheii@gmail.com>
2023-08-02 14:38:53 +0300

    add global prefix and update port

--- admin/src/config/validation.config.schema.ts
+++ admin/src/config/validation.config.schema.ts
@@ -1,6 +1,8 @@
 import * as Joi from 'joi';
 
 export const configValidationSchema = Joi.object({
+  PORT: Joi.number().required(),
+  GLOBAL_PREFIX: Joi.string().required(),
   DB_HOST: Joi.string().required(),
   DB_PORT: Joi.number().default(5432).required(),
   DB_USERNAME: Joi.string().required(),
