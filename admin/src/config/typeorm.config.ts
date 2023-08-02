24de7149e4043fd4d41a8ded3111f673d37547da
MohsenKhashei <m.khasheii@gmail.com>
2023-08-02 12:14:23 +0300

    config typeorm

---
+++ admin/src/config/typeorm.config.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: this.configService.get('DB_HOST'),
+      port: this.configService.get<number>('DB_PORT'),
+      username: this.configService.get('DB_USERNAME'),
+      password: this.configService.get('DB_PASSWORD'),
+      database: this.configService.get('DB_NAME'),
+      entities: [],
+      synchronize: true,
+    };
+  }
+}
