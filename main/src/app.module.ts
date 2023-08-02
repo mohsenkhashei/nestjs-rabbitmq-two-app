bb6c72518182773605561151d2e96cdc2d5e20d3
MohsenKhashei <m.khasheii@gmail.com>
2023-08-02 18:06:22 +0300

    add mongoose config

--- main/src/app.module.ts
+++ main/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfigService } from './config/mongoose.config.service';
+import { configValidationSchema } from './config/validation.config.schema';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`./src/config/.env`],
+      validationSchema: configValidationSchema,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: MongooseConfigService,
+    }),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
