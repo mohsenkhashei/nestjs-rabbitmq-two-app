05be1f0c1bf3b3c436c55608a6418904a62287c3
MohsenKhashei <m.khasheii@gmail.com>
2023-08-02 12:14:55 +0300

    add config to app module

--- admin/src/app.module.ts
+++ admin/src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { configValidationSchema } from './config/config.schema';
+import { TypeOrmConfigService } from './config/typeorm.config';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`./src/config/.env`],
+      validationSchema: configValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+    }),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
